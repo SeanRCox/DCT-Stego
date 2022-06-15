c68ad4bd52a79aac8f9d6bfed341c84a0b617652
kevinhig <kevin@LAPTOP-HRDR3GDN.localdomain>
2022-06-15 13:10:20 -0400

    added resize_image function

--- image_processing.py
+++ image_processing.py
@@ -1,15 +1,26 @@
 from PIL import Image
 import numpy as np
 
+def resize_image(img):
+    # takes an image and resizes it if not multiple of 8
+    # returns 1 if it resized
+    im = Image.open(img)
+    width, height = im.size
+    if (closest_factor(width,8) != width | closest_factor(height,8) != height) :
+        im = im.resize((closest_factor(width,8), closest_factor(height,8)))
+        im.save(img)
+        return 1
+    return 0
+
 def closest_factor(num, fac): #Function for finding closest value that is a factor, used for resize on w and h
     for i in range(fac):
-        print(num)
         if (num + i) % fac == 0:
             num += i
             break
         elif (num - i) % fac == 0:
             num -= i
             break
+    print(num)
     return num
 
 def get_image_data(file) : # opens image and returns the RGB data as a numpy array

afcbad16fc80294fda7d0568679ea9ceff699b15
Sean <Sean_Cox@student.uml.edu>
2022-06-18 19:24:05 -0400

    im class, rgb to ycbcr

--- dct.py
+++ dct.py
@@ -1,8 +1,23 @@
 from scipy.fftpack import dct
 from image_processing import *
+import cv2
 
-img = get_image_data("sloth.jpg")
-data = create_blocks(img)
-print(len(data))
-print(data[0])
-print(dct(data[0]))
+stego_image = im("sloth.jpg")
+#print(stego_image.Y)
+print(len(stego_image.Y))
+print(stego_image.Y[10000])
+dct_blocks = [dct(block) for block in stego_image.Y]
+print(len(dct_blocks))
+print(dct_blocks[10000])
+
+quantized_blocks = []
+for n in dct_blocks:
+
+    new_block = np.zeros((8,8))
+    for i in range(8):
+        for j in range(8):
+            new_block[i][j] = int(float(n[i][j] * quant_table[i][j]))
+
+    quantized_blocks.append(new_block)
+
+print(quantized_blocks[10000])
--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,27 @@
 from PIL import Image
 import numpy as np
 
+quant_table = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                       [12, 12, 14, 19, 26, 58, 60, 55],
+                       [14, 13, 16, 24, 40, 57, 69, 56],
+                       [14, 17, 22, 29, 51, 87, 80, 62],
+                       [18, 22, 37, 56, 68, 109, 103, 77],
+                       [24, 36, 55, 64, 81, 104, 113, 92],
+                       [49, 64, 78, 87, 103, 121, 120, 101],
+                       [72, 92, 95, 98, 112, 100, 103, 99]])
+
+class im :
+    def __init__(self, image) :
+        img = get_image_data(image) #getting image data
+        if img.shape[0] % 8 != 0 or img.shape[1] % 8 != 0 : #resizing image if need be
+            resize_image(img)
+        #img = RGBtoYCbCr(img) #converting image from RGB to YCbCr
+
+        #creating 8x8 blocks for Luminance, Chroma Blue, Chroma Red
+        self.Y = create_blocks(img[:,:,0]) 
+        self.Cb = create_blocks(img[:,:,1])
+        self.Cr = create_blocks(img[:,:,2])
+
 def resize_image(img):
     # takes an image and resizes it if not multiple of 8
     # returns 1 if it resized
@@ -24,7 +45,7 @@ def closest_factor(num, fac): #Function for finding closest value that is a fact
     return num
 
 def get_image_data(file) : # opens image and returns the RGB data as a numpy array
-    image = Image.open('sloth.jpg')
+    image = Image.open(file)
     return np.asarray(image)
 
 def create_blocks(image) : #turn the image into 8x8 blocks of the RGB values
@@ -34,6 +55,9 @@ def create_blocks(image) : #turn the image into 8x8 blocks of the RGB values
             blocks.append(horiz_slice)
     return blocks
 
-def make_array(blocks) : # puts the 8x8 blocks into an array so the DCT coefficents can be calculated
-    pass
+def RGBtoYCbCr(im) :
+    xform = np.array([[.299, .587, .114], [-.1687, -.3313, .5], [.5, -.4187, -.0813]])
+    ycbcr = im.dot(xform.T)
+    ycbcr[:,:,[1,2]] += 128
+    return np.uint8(ycbcr)
 

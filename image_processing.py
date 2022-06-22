48a7f6e5a3d8479899f0ea1905ddf6f00c799c3b
Sean <Sean_Cox@student.uml.edu>
2022-06-21 21:58:52 -0400

    fixed dct, zig zag function

--- image_processing.py
+++ image_processing.py
@@ -14,22 +14,21 @@ class im :
     def __init__(self, image) :
         img = get_image_data(image) #getting image data
         if img.shape[0] % 8 != 0 or img.shape[1] % 8 != 0 : #resizing image if need be
-            resize_image(img)
-        #img = RGBtoYCbCr(img) #converting image from RGB to YCbCr
+            resize_image(image)
+            img = get_image_data(image)
+        img = rgb_to_ycbcr(img) #converting image from RGB to YCbCr
 
         #creating 8x8 blocks for Luminance, Chroma Blue, Chroma Red
-        self.Y = create_blocks(img[:,:,0]) 
-        self.Cb = create_blocks(img[:,:,1])
-        self.Cr = create_blocks(img[:,:,2])
+        self.layers = [create_blocks(img[:,:,0]), create_blocks(img[:,:,1]), create_blocks(img[:,:,2])]
 
-def resize_image(img):
+def resize_image(image):
     # takes an image and resizes it if not multiple of 8
     # returns 1 if it resized
-    im = Image.open(img)
+    im = Image.open(image)
     width, height = im.size
     if (closest_factor(width,8) != width | closest_factor(height,8) != height) :
         im = im.resize((closest_factor(width,8), closest_factor(height,8)))
-        im.save(img)
+        im.save(image)
         return 1
     return 0
 
@@ -50,14 +49,14 @@ def get_image_data(file) : # opens image and returns the RGB data as a numpy arr
 
 def create_blocks(image) : #turn the image into 8x8 blocks of the RGB values
     blocks = []
-    for vert_slice in np.vsplit(image, int(image.shape[0] / 8)):
-        for horiz_slice in np.hsplit(vert_slice, int(image.shape[1] / 8)):
-            blocks.append(horiz_slice)
+    for vertical_block in np.vsplit(image, int(image.shape[0] / 8)):
+        for horizontal_block in np.hsplit(vertical_block, int(image.shape[1] / 8)):
+            blocks.append(horizontal_block)
     return blocks
 
-def RGBtoYCbCr(im) :
+def rgb_to_ycbcr(image) :
     xform = np.array([[.299, .587, .114], [-.1687, -.3313, .5], [.5, -.4187, -.0813]])
-    ycbcr = im.dot(xform.T)
+    ycbcr = image.dot(xform.T)
     ycbcr[:,:,[1,2]] += 128
     return np.uint8(ycbcr)
 
